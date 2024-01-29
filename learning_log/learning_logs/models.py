a3c83c76996fb6c14e4e862ae9e233ca19d92ab3
Donato Souza <donatosouza33@gmail.com>
2024-01-29 16:33:01 -0300

    Definindo o modelo para os tipos de entrada que os usuários podem criar em seus registros de aprendizado.

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -5,8 +5,17 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto."""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    verbose_name_plural = 'entries'
+    
 def __str__(self):
     """Devolve uma representação em string do modelo."""
-    return self.text
+    return self.text[:50] + "..."
 
 # Create your models here.
