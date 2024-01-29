65e0d735276a2e2fd217870f9f5bf092c787d6ad
Donato Souza <donatosouza33@gmail.com>
2024-01-29 17:06:17 -0300

    Usando Shell de Django.

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -5,17 +5,21 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        """Devolve uma representação em string do modelo."""
+        return self.text
 class Entry(models.Model):
     """Algo específico aprendido sobre um assunto."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-
-class Meta:
-    verbose_name_plural = 'entries'
-
-def __str__(self):
-    """Devolve uma representação em string do modelo."""
-    return self.text[:50] + "..."
+    class Meta:
+        verbose_name_plural = 'entries'
+    def __str__(self):
+        """Devolve uma representação em string do modelo."""
+        if(len(self.text) > 50):
+            return self.text[:50] + "..."
+        else:
+            return self.text[:50]
 
 # Create your models here.
